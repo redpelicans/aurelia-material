e3a92bc53c6e5776770eb0936cc1fd7a52802152
Eric <eric.basley@redpelicans.com>
2015-07-16 15:35:12 +0000

    first test

--- config.js
+++ config.js
@@ -19,7 +19,6 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.18",
     "google/material-design-lite": "github:google/material-design-lite@1.0.0",
-    "material-design-lite": "npm:material-design-lite@1.0.0",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
@@ -30,9 +29,6 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:material-design-lite@1.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
