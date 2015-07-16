2aa9668c688aa99c87476157abcee6a3d688a2c4
Eric <eric.basley@redpelicans.com>
2015-07-16 15:17:33 +0000

    ready to gulp with you ...

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # aurelia-material
 Aurelia wrapper for [Material Design Lite](http://www.getmdl.io).
+
+
+### Install
+
+```
+  $ jspm install github:google/material-design-lite -o "{main: './material.js'}"
+```
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
+var babel = require('gulp-babel');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var runSequence = require('run-sequence');
+var path = require('path');
+var paths = {
+    source:'src/**/*.js'
+  , dist: 'dist'
+  , sourceRoot: path.join(__dirname, 'src'),
+}
+
+
+// need this options to use decorators
+var compilerOptions = {
+  stage: 0,
+  optional: [
+    "es7.decorators",
+    "regenerator",
+    "asyncToGenerator",
+    "es7.classProperties",
+    "es7.asyncFunctions"
+  ]
+};
+
+
+gulp.task('build-client', function () {
+  return gulp.src(paths.source)
+    .pipe(plumber())
+    .pipe(changed(paths.dist, {extension: '.js'}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(babel(compilerOptions))
+    .pipe(sourcemaps.write('.', { includeContent: true, sourceRoot: paths.sourceRoot}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build-html', function () {
+  return gulp.src(paths.source)
+    .pipe(changed(paths.dist, {extension: '.html'}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task('build', function(callback) {
+  return runSequence(
+    'clean',
+    ['build-client', 'build-html'],
+    callback
+  );
+});
+
+gulp.task('clean', function() {
+ return gulp.src([paths.dist])
+    .pipe(vinylPaths(del));
+});
+
+
+gulp.task('default', ['build']);
+
