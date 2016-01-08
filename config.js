e888ddb2a665f54b42f98e1cb78ad8e4787a698e
Eric Swann <eric.swann@q2ebanking.com>
2016-01-08 17:16:50 -0600

    updated the library package

--- config.js
+++ config.js
@@ -16,11 +16,29 @@ System.config({
     "babel": "npm:babel-core@5.7.2",
     "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.18",
-    "google/material-design-lite": "github:google/material-design-lite@1.0.0",
+    "eric-swann-q2/aurelia-material": "github:eric-swann-q2/aurelia-material@master",
+    "google/material-design-lite": "github:google/material-design-lite@1.0.6",
     "jquery": "github:components/jquery@2.1.4",
-    "lodash": "npm:lodash@3.10.0",
+    "lodash": "npm:lodash@3.10.1",
+    "github:eric-swann-q2/aurelia-material@master": {
+      "google/material-design-lite": "github:google/material-design-lite@1.0.6",
+      "jquery": "github:components/jquery@2.1.4",
+      "lodash": "npm:lodash@3.10.1"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.7.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -30,8 +48,18 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:lodash@3.10.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
